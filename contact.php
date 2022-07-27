f16bf5b8265ddfd6bcbcf1488bd102970f51a8e3
Andrey Komov <andrewkomov@gmail.com>
2022-07-27 13:52:01 +0300

    array

--- contact.php
+++ contact.php
@@ -4,19 +4,12 @@
 <head>
   <title>Contact details</title>
   <meta charset="utf-8" />
-  <link rel="stylesheet" href="./styles/style.css" />
+  <link rel="stylesheet" href="styles/contact.css" />
 </head>
-
-<body style="background-color: #a68130" id="contact-body">
-
-  <div id="header">
-  </div>
-
+<div id="slogan">Act, Non Verba</div>
+<body class="contact-body">
   <div id="content">
-    <!-- Заголовок -->
     <h1>Feedback</h1>
-    <!-- Заголовок -->
-    <!-- Область основного контента -->
       <?
       $size = ini_get("post_max_size");
       $last = substr($size, -1);
@@ -25,7 +18,7 @@
       $mB = $kB*1024;
       $gB = $mB*1024;
       $result = "";
-      switch ($last) {
+      switch (strtoupper($last)) {
           case "K":
               $result = $allNumbers * $kB; break;
           case "M":
@@ -42,23 +35,24 @@
     <form action='' method='post'>
       <label>Subject: </label>
       <br />
-      <input style="background-color: #2A4F6F" name='subject' type='text' size="50" />
+      <input name='subject' type='text' size="50" />
       <br />
       <label>Content: </label>
       <br />
-      <textarea style="background-color: #2A4F6F" name='body' cols="50" rows="10"></textarea>
+      <textarea  name='body' cols="50" rows="10"></textarea>
       <br />
       <br />
-      <input style=" font-family: AIGDT; background-color: #B2BCC6; width: 150px;" type='submit' value='Submit' />
+      <button class="submit-button" " type='submit' value='Submit'>
+       SUBMIT
+      </button>
     </form>
-      <p>Data send - max volume <?= $result ?> byte.</p>
-    <!-- Область основного контента -->
+    <p id="data">Data send - max volume <?= $result ?> byte.</p>
   </div>
   <br>
   <br>
   <div id="nav">
-    <h2 style="color:#3b3b9b;">Navigation</h2>
-    <!-- Меню -->
+      <img src="Rabbit.png" alt="Logo" class="logo" />
+    <h2>Navigation Menu</h2>
     <ul>
       <li><a href='index.php'>Home</a>
       </li>
@@ -71,13 +65,10 @@
       <li><a href='calc.php'>Calculator</a>
       </li>
     </ul>
-    <!-- Меню -->
   </div>
   <br>
   <div id="footer">
-    <!-- Нижняя часть страницы -->
     &copy; Rabbit Master, 2022
-    <!-- Нижняя часть страницы -->
   </div>
 </body>
 
